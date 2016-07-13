811430081a2ef0ca03d9f1e95f32d0aed439692a
Wouter de Vos <wouter@codaisseur.com>
2016-07-13 13:20:28 +0200

    Basic Chat API with Feathers

---
+++ src/services/message/hooks/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const restrictToSender = require('./restrict-to-sender');
+
+const process = require('./process');
+
+const globalHooks = require('../../../hooks');
+const hooks = require('feathers-hooks');
+const auth = require('feathers-authentication').hooks;
+
+const populateSender = hooks.populate('sentBy', {
+  service: 'users',
+  field: 'userId'
+});
+
+exports.before = {
+  all: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated()
+  ],
+  find: [],
+  get: [],
+  create: [process()],
+  update: [hooks.remove('sentBy'), restrictToSender()],
+  patch: [hooks.remove('sentBy'), restrictToSender()],
+  remove: [restrictToSender()]
+};
+
+exports.after = {
+  all: [],
+  find: [populateSender],
+  get: [populateSender],
+  create: [populateSender],
+  update: [],
+  patch: [],
+  remove: []
+};
